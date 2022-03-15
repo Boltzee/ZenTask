349527bbfdd54bae291da88e3a80e42f6e3d3757
Kiran Tirunagiri <me190003064@iiti.ac.in>
2022-03-16 03:15:06 +0530

    imported the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import db from "./firebase";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar/NavBar";
 import Homepage from "./components/Homepage/Homepage";
 import Not_found from "./components/not_found/Not_found";
 import Footer from "./components/footer/Footer";
@@ -10,6 +11,7 @@ function App() {
     return (
         <div className="container-fluid p-0">
             <Router>
+                <NavBar />
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="*" element={<Not_found />} />
