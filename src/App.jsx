bb8fb64b1fa7f096c83a1d8261659890110ad9c7
Kiran Tirunagiri <me190003064@iiti.ac.in>
2022-03-16 18:56:06 +0530

    Removed the footer from the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar/NavBar";
 import Homepage from "./components/Homepage/Homepage";
 import Not_found from "./components/not_found/Not_found";
-import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
                     <Route path="/" element={<Homepage />} />
                     <Route path="*" element={<Not_found />} />
                 </Routes>
-                <Footer />
             </Router>
         </div>
     );
